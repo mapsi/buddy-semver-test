cd9038b2c02e90ff0db4c8c5fd80ef01dea6bcca
Angel Psiakis <angel@mml.rocks>
2020-08-28 20:53:23 +0100

    Merged in hotfix/GXR-2621-broke

--- git.sh
+++ git.sh
@@ -3,13 +3,12 @@
 STRING=$(git reflog -1 | sed 's/^.*: //')
 
 if echo $STRING | grep -q "Merged in hotfix/"; then
-  TAG=$(./semver.sh patch)
+  TAG="$(./semver.sh patch)"
 elif echo $STRING | grep -q "Merged in release/"; then
-  TAG=$(./semver.sh minor)
+  TAG="$(./semver.sh minor)"
 else
   TAG=""
 fi
-
 
 if [[ -n $TAG ]]; then
   echo "New tag: $TAG"
