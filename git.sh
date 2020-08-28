f42eee706667513723776595eca082b463db3485
Angel Psiakis <angel@mml.rocks>
2020-08-28 17:54:52 +0100

    Merged in release/

--- git.sh
+++ git.sh
@@ -3,11 +3,14 @@
 STRING=$(git reflog -1 | sed 's/^.*: //')
 
 if echo $STRING | grep -q "Merged in hotfix/"; then
-TAG=$(./semver.sh patch)
+  TAG=$(./semver.sh patch)
 elif echo $STRING | grep -q "Merged in release/"; then
-TAG=$(./semver.sh minor)
+  TAG=$(./semver.sh minor)
 else
-TAG=""
+  TAG=""
 fi
 
-git tag $TAG
+
+if [[ -n $VAR ]]; then
+  git tag $TAG
+fi
