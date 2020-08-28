d43667e8ed2a4cf0ee4564c2d12e8de076499e07
Angel Psiakis <angel@mml.rocks>
2020-08-28 21:28:16 +0100

    hotfix/GXR-2621-

--- semver.sh
+++ semver.sh
@@ -44,6 +44,8 @@ if echo $STRING | grep -q "Merged in hotfix/"; then
   TAG="$(semver patch)"
 elif echo $STRING | grep -q "Merged in release/"; then
   TAG="$(semver minor)"
+  echo "here"
+  echo $TAG
 else
   TAG=""
 fi
