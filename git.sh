b6935cb75d4598af5ba174720265a49bbe6b4996
Angel Psiakis <angel@mml.rocks>
2020-08-28 15:02:00 +0100

    tag

---
+++ git.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+STRING=$(git reflog -1 | sed 's/^.*: //')
+
+if echo $STRING | grep -q "Merged in hotfix/"; then
+TAG=$(./semver.sh patch)
+elif echo $STRING | grep -q "Merged in release/"; then
+TAG=$(./semver.sh minor)
+else
+TAG=""
+fi
+
+EXPORT TAG
