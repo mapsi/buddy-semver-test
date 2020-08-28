109b89d5106ef49684d99ba0e3fc141e4d881979
Angel Psiakis <angel@mml.rocks>
2020-08-28 15:28:23 +0100

    Merged in hotfix/GXR-26

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -22,7 +22,8 @@ class ArticlesController extends Controller
     public function index()
     {
         $data = $this->cacheResponseData(function () {
-            $service = $this->service;
+            $service =
+            $this->service;
 
             $siteLeadQuery = $service->newSearch()
                 ->setTagIds([Article::ENTITY_TYPE_TAG_ID, ArticleCategory::TAG_ID_SITE_LEAD, ArticleCategory::TAG_ID_FRONT_PAGE_PROMOTED])
